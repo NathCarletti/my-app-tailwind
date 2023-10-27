47fad34d44dcd530870b51d14841ab239a594a2d
Nathalia <ncsoares2@gmail.com>
2023-10-27 13:46:21 -0300

    starting to use api, axios and fetch

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,6 +1,8 @@
 import { useNavigation } from "@react-navigation/native";
 import { View, Text, Button, FlatList, SectionList } from "react-native";
 import { Card } from "../../components/Card";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const DATA=[
     {
@@ -32,19 +34,37 @@ const DATASec=[
 ]
 
 export default function Home(){
+    const [data, setData] = useState([]);
 
     const navigation = useNavigation();
+
+    // useEffect(()=>{
+    //     fetch('https://jsonplaceholder.typicode.com/users').then((res)=>res.json().then((json => setData(json))))
+    //     .catch(err=> console.warn(err)); //pega o res.json e entao o coloca dentro de data
+    // },[]);
+
+    useEffect(()=>{
+        axios.get('https://jsonplaceholder.typicode.com/users').then((res => setData(res.data)))
+        .catch(err=>console.warn(err));
+    },[]);
+
     return(
         <View className="flex-1 items-center justify-center">
             <Text className = 'text-red-500'>Bem-vindo a HOME</Text>
             <Button title="Ir para detalhes" onPress={()=>{
                 navigation.navigate('Details')
             }}/>
-            {/* <Card title={'Como trabalhar com listas'} image={'https://github.com/Caio18-cosenza.png'} />  */}
 
+            <FlatList data={data} renderItem={({index,item})=>(
+                <Card title={`Nome: ${item.name}`} image={'https://github.com/Caio18-cosenza.png'} />
+            )} keyExtractor={(item)=>item.title} />
+
+
+            {/* <Card title={'Como trabalhar com listas'} image={'https://github.com/Caio18-cosenza.png'} />  */}
+            {/* 
             <FlatList data={DATA} renderItem={({index,item})=>(
                 <Card title={item.title} image={item.bannerURL} />
-            )} keyExtractor={(item)=>item.title} /> 
+            )} keyExtractor={(item)=>item.title} />  */}
 
             {/* <SectionList sections={DATASec} renderItem={({index, item})=>(
                 <Card title={item} image={item} />
